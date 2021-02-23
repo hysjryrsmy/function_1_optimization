977828ea17ab4e998cd33b5eb436102f9c3ff9d3
yiyi <1484491854a@gmail.com>
2021-02-23 17:52:13 +0900

    程序的优化

---
+++ test.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//放置函数的声明
+//#ifndef __TEST_H__
+//#define __TEST_H__
+////函数的声明
+//int Add(int x, int y);
+//#endif //__TEST_H__
+
+
+#include "add.h"
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("%d\n", sum);
+	return 0;
+}
